dbbcd6ca1796d97a8b261e5e0d0ab9e386d0a976
Malenia <82274616+TheMalenia@users.noreply.github.com>
2024-01-24 15:30:35 +0330

    Update rand.py

--- rand.py
+++ rand.py
@@ -10,3 +10,13 @@ def rand01(mult=2**15-19,mod=2**31-1,base=2**10,cst=1,size=1000):
         s[i]=x/mod
         i+=1
     return s
+#make a random number between a and b
+def random(a,b,base=2**10,size=1):
+    s=rand01(base=base,size=size)
+    i=0
+    while i<len(s):
+        if s[i]==1:
+            s[i]=0
+        s[i]=a+(b-a)*s[i]
+        i+=1
+    return s
